f1d81a3459642ef82d3122c24b2cd99b4c2855a1
ahmed <ahmedmohamedfoad109@gmail.com>
2024-12-25 23:45:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import { ErrorBoundary } from 'react-error-boundary';
-function ErrorFallback({error}) {
-    return (
-      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-        <strong className="font-bold">Oops! Something went wrong:</strong>
-        <pre className="mt-2 text-sm">{error.message}</pre>
-      </div>
-    );
-  }
+
+function ErrorFallback({ error }) {
+  return (
+    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <strong className="font-bold">Oops! Something went wrong:</strong>
+      <pre className="mt-2 text-sm">{error.message}</pre>
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <main className=' mx-auto'>
-      <Navbar/>
-      <Hero/>
-    </main>
+      <main className=' mx-auto'>
+        <Navbar/>
+        <Hero/>
+      </main>
     </ErrorBoundary>
   )
 }
 
 export default App
+
