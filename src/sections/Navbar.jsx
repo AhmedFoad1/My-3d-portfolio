44b283b327f01266e48c7d8be63a720e3b8a6681
ahmed <ahmedmohamedfoad109@gmail.com>
2025-01-05 23:47:57 +0200

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -8,7 +8,7 @@ const NavItems = () => {
         <ul className='nav-ul'>
             {navLinks.map(({id, href , name}) => (
                <li key={id} className='nav-li'>
-                   <a href={href} className='nav-li_a' onClick={()=>{}}>{name}</a>
+                   <a href={href} className='nav-li_a'>{name}</a>
                </li>
                
             ))}
@@ -22,7 +22,7 @@ const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => setIsOpen((prevIsOpen => !prevIsOpen));
+    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
     
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'> 
